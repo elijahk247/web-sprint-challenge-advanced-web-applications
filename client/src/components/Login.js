03b9c4fd9389b0f33bd92a5a8e7aedcb86fbc2ba
Elijah Kim <elijahkim@Elijahs-MacBook-Pro.local>
2020-09-18 11:40:42 -0400

    Added axiosWithAuth, finishing up the login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { axiosWithAuth } from '../utilities/axiosWithAuth';
+import { useHistory } from 'react-router-dom';
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [user, setUser] = useState({
+    credentials: {
+      username: '',
+      password: ''
+    }
+  });
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+
+    setCredentials({
+      ...credentials,
+      [name]: value
+    });
+  };
+
+  const login = e => {
+    e.preventDefault();
+
+  }
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div className='login-container'>
+      <form onSubmit={login}>
+        <label>Username: </label>
+        <input 
+          type='text'
+          name='username'
+          value={credentails}
+        />
+      </form>
+    </div>
   );
 };
 
