825a89aaf4e4ae3b723fb5d2d18660a2011b4b99
Elijah Kim <elijahkim@Elijahs-MacBook-Pro.local>
2020-09-18 12:04:22 -0400

    Finished authentification and routing between the pages, need to work on stage 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+
+import BubblePage from './components/BubblePage';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <nav>
+          <Link to='/'>Login</Link>
+          <Link to='/bubblepage'>Bubble Page</Link>
+        </nav>
+
+        <Switch>
+          <Route exact path="/" component={Login} />
+            {/* 
+              Build a PrivateRoute component that will 
+              display BubblePage when you're authenticated 
+            */}
+          <PrivateRoute exact path = '/bubblepage' component={BubblePage} />
+        </Switch>
       </div>
     </Router>
   );
---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const token = window.localStorage.getItem('token');
+
+  return (
+    <Route 
+      {...rest}
+      render={props => 
+        token ? 
+        (<Component {...props} />) :
+        (<Redirect to='/login' />)
+      }
+    />
+  )
+}
+
+export default PrivateRoute;
